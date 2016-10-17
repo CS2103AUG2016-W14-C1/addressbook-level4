b4cf88abfd1fdc4867914eac75cb0d22e33fcadf
aryrobocode <ang.ray.yan@gmail.com>
2016-10-18 06:01:48 +0800

    Update UI to light theme (partial). Allow activities to be sorted by DateTime

--- src/main/java/seedu/manager/model/activity/Activity.java
+++ src/main/java/seedu/manager/model/activity/Activity.java
@@ -4,7 +4,7 @@ package seedu.manager.model.activity;
  * Abstract class for all types of activity in Remindaroo.
  */
 
-public abstract class Activity implements ReadOnlyActivity {
+public abstract class Activity implements ReadOnlyActivity, Comparable<Activity> {
 	protected String name;
 	protected Status status;
 	
@@ -50,4 +50,29 @@ public abstract class Activity implements ReadOnlyActivity {
 	           (o instanceof Activity &&
 	            this.name.equals(((Activity)o).name));
 	}
+	
+    @Override
+    public int compareTo(Activity other) {
+        if (this instanceof FloatingActivity) {
+            return 1;
+        } else if (other instanceof FloatingActivity) {
+            return -1;
+        // Comparison between 2 deadlines
+        } else if (this instanceof DeadlineActivity && other instanceof DeadlineActivity) {
+            return (int)(((DeadlineActivity) this).getDateTime().getTime() - ((DeadlineActivity) other).getDateTime().getTime());
+        // Comparisons between a deadline and an event
+        } else if (this instanceof EventActivity && other instanceof DeadlineActivity) {
+            return (int)(((EventActivity) this).getDateTime().getTime() - ((DeadlineActivity) other).getDateTime().getTime());     
+        } else if (this instanceof DeadlineActivity && other instanceof EventActivity) {
+            return (int)(((DeadlineActivity) this).getDateTime().getTime() - ((EventActivity) other).getDateTime().getTime());   
+        // Comparisons between 2 events
+        } else {
+           long startTimeDifference = ((EventActivity) this).getDateTime().getTime() - ((DeadlineActivity) other).getDateTime().getTime();     
+           if (startTimeDifference == 0) {
+               return (int)(((EventActivity) this).getEndDateTime().getTime() - ((EventActivity) other).getEndDateTime().getTime());
+           } else {
+               return ((int) startTimeDifference);
+           }
+        }
+    }
 }
