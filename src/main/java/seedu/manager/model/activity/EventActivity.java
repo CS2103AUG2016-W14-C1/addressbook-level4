a6943f9409c96c002dd5385ef62e184013094ec0
Lee Han Cheng <lhchchc@gmail.com>
2016-10-18 19:16:32 +0800

    Refactor EventActivity message to constant

--- src/main/java/seedu/manager/model/activity/EventActivity.java
+++ src/main/java/seedu/manager/model/activity/EventActivity.java
@@ -8,6 +8,7 @@ import seedu.manager.commons.exceptions.IllegalValueException;
  */
 
 public class EventActivity extends Activity {
+    public static final String MESSAGE_DATE_CONSTRAINTS = "Event has already ended before it starts.";
     private AMDate dateTime;
     private AMDate endDateTime;
 
@@ -27,7 +28,7 @@ public class EventActivity extends Activity {
         this.dateTime = new AMDate(newStartDateTime);
         this.endDateTime = new AMDate(newEndDateTime);
         if (this.dateTime.getTime() > this.endDateTime.getTime()) {
-            throw new IllegalValueException("Event has already ended before it starts.");
+            throw new IllegalValueException(MESSAGE_DATE_CONSTRAINTS);
         }
     }
     
